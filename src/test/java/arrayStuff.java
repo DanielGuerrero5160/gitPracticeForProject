310d0405d727c1fe07b0b2451530ae0615dbf4bf
DanielGuerrero5160 <daniel.guerrero5160@gmail.com>
2023-04-09 16:08:09 -0400

    ArrayStuff First Draft

---
+++ src/test/java/arrayStuff.java
@@ -0,0 +1,8 @@
+public class arrayStuff {
+    public static void main(String[] args) {
+        String[] arr={"Hello","My","Name","Is","Daniel"};
+        for(String s:arr){
+            System.out.println(s);
+        }
+    }
+}
